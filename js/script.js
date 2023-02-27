30f5dbbe3578f6f3515bb8f5c0a5d58c94dea217
DavideAtzo <atzo9477@gmail.com>
2023-02-27 16:25:00 +0100

    prova click

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+// dichiaro variabili 
+const imgArray = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp',];
+let img = '';
+let active = 0;
+
+// prendo elemento html e la inserisco in una variabile 
+const slider = document.querySelector('.slider');
+const rowUp = document.querySelector('.row-up');
+const rowDown = document.querySelector('.row-down');
+
+rowUp.addEventListener('click',
+function(){
+    alert('img precedente')
+})
