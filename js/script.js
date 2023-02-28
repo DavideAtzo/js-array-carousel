2e38f406af6874620aa988fbde4488627708996b
DavideAtzo <atzo9477@gmail.com>
2023-02-28 10:52:31 +0100

    inizio bonus 3

--- js/script.js
+++ js/script.js
@@ -4,22 +4,40 @@
 const imgArray = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp'];
 let imgContent = '';
 let active = 0;
+let imgPreview = '';
+
+/* <div class="prev-img"><img src="img/02.webp" alt="img"></div>
+    <div class="prev-img"><img src="img/02.webp" alt="img"></div>
+    <div class="prev-img"><img src="img/02.webp" alt="img"></div>
+    <div class="prev-img"><img src="img/02.webp" alt="img"></div>
+    <div class="prev-img"><img src="img/02.webp" alt="img"></div> */
+
 
 // prendo elemento html e la inserisco in una variabile 
 const slider = document.querySelector('.slider');
 // ciclo for che fa inserire 5 volte gli elementi con img e classi personalizzate 
 for (let i = 0; i < imgArray.length; i++) {
     console.log(imgArray[i]);
-    imgContent += `<div class="box-img"><img src="img/${imgArray[i]}" alt=""></div>`;
+    imgContent += `<div class="box-img"><img src="img/${imgArray[i]}" alt="img"></div>`;
 }
 // inserisco i precedenti elementi nel contenitore html 
 slider.innerHTML += imgContent;
+
+
 // aggiungo la classe show nell elemento 
 document.querySelector('.box-img').classList.add('show');
-// dichiaro altre variabili 
+// dichiaro altre variabili prendendo dall'html
 const prev = document.querySelector('.row-up');
 const next = document.querySelector('.row-down');
 const boxesImg = document.querySelectorAll('.box-img');
+const preview = document.querySelector('.preview')
+
+for (let i = 0; i < imgArray.length; i++) {
+    console.log(imgArray[i]);
+    imgPreview += `<div class="prev-img"><img src="img/${imgArray[i]}" alt="img"></div>`;
+}
+preview.innerHTML += imgPreview;
+
 // evento click img precedente 
 prev.addEventListener('click',
     function () {
